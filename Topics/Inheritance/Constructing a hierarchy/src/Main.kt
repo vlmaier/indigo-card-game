77ddb714b7bb9a2b6b2ba37497ae0353747ce17a
Vladas Maier <vladas.mr@gmail.com>
2023-03-18 19:58:15 +0100

    Add "Inheritance" topic

---
+++ Topics/Inheritance/Constructing a hierarchy/src/Main.kt
@@ -0,0 +1,8 @@
+open class Animal
+open class Mammal : Animal()
+open class Fish : Animal()
+open class Bird : Animal()
+class Lion : Mammal()
+class Salmon : Fish()
+class Carp : Fish()
+class Eagle : Bird()
